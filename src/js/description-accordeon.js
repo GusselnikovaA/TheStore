701d5938bb0addcfc61b91915f9195c4636605ab
Anna <annanac1993@gmail.com>
2020-07-06 22:08:58 +0300

    accordeon

--- src/js/description-accordeon.js
+++ src/js/description-accordeon.js
@@ -1,5 +1,17 @@
-const items = document.querySelectorAll('.description-list__item_view');
+const accordeonItems = document.querySelectorAll('.description-list__item_view');
 
-items.forEach(item => {
-  item.addEventListener('click', (e) => this.classList.add('active'));
+
+accordeonItems.forEach(item => {
+  item.addEventListener('click', (e) => {
+    const accordeonIcon = item.querySelector('.description_open');
+    const accordeonContent = item.nextElementSibling;
+
+    if (accordeonContent.classList.contains('active')) {
+      accordeonIcon.innerHTML = '<use xlink:href="#plus"></use>'
+    } else {
+      accordeonIcon.innerHTML = '<use xlink:href="#minus"></use>'
+    }
+
+    accordeonContent.classList.toggle('active');
+  })
 });
