1bb13644e268fe1f146591ecdc94f50e7e56abba
Anna <annanac1993@gmail.com>
2020-07-06 23:46:18 +0300

    Adaptive

--- src/js/slider-settings.js
+++ src/js/slider-settings.js
@@ -1,22 +1,30 @@
 $('.recommend-slider').slick({
-  slidesToShow: 3,
+  slidesToShow: 3.3,
   slidesToScroll: 1,
   infinite: false,
   speed: 2000,
   dots: true,
   prevArrow: $('.recommend-arrows__left'),
   nextArrow: $('.recommend-arrows__right'),
-  responsive: [{
+  responsive: [
+    // {
+    //   breakpoint: 1300,
+    //   settings: {
+    //     slidesToShow: 3,
+    //     arrows: false
+    //   }
+    // },
+    {
       breakpoint: 1000,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 2.3,
         arrows: false
       }
     },
     {
-      breakpoint: 568,
+      breakpoint: 650,
       settings: {
-        slidesToShow: 1
+        slidesToShow: 1.2
       }
     }]
 });
