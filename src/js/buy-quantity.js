5543d68bf08ceef700c394fef1fb283b154361ec
GusselnikovaA <annanac1993@gmail.com>
2020-07-07 17:20:20 +0300

    Adaptive. cart count. recomment title mobile. change color in buy quantity. retina img test

--- src/js/buy-quantity.js
+++ src/js/buy-quantity.js
@@ -1,17 +1,17 @@
 const minus = document.querySelector('.buy-quantity__minus');
 const plus = document.querySelector('.buy-quantity__plus');
+const minusIcon = document.querySelector('.buy-quantity__minus-icon');
+const plusIcon = document.querySelector('.buy-quantity__plus-icon');
 const amount = document.querySelector('.buy-quantity__value');
-
-// if(amount.value == '') {
-//   amount.value = 1
-// }
 
 minus.addEventListener('click', (e) => {
   e.preventDefault();
 
   if(amount.value > 1) {
     amount.value--;
-  }
+  };
+
+  colorChange(amount.value);
 });
 
 plus.addEventListener('click', (e) => {
@@ -19,15 +19,36 @@ plus.addEventListener('click', (e) => {
 
   if(amount.value < 10) {
     amount.value++;
-  }
+  };
+
+  colorChange(amount.value);
 });
 
 amount.addEventListener('keyup', (e) => {
   if(amount.value < 1) {
     amount.value = 1;
-  }
+  };
 
   if(amount.value > 10) {
     amount.value = 10;
-  }
+  };
+
+  colorChange(amount.value);
 })
+
+function colorChange(value) {
+  if (value == 1) {
+    minusIcon.style.stroke = '#BDBDBD';
+    plusIcon.style.stroke = '#212121';
+  }
+
+  if (value >= 2 && value <= 9 ) {
+    minusIcon.style.stroke = '#212121';
+    plusIcon.style.stroke = '#212121';
+  }
+
+  if (value == 10) {
+    minusIcon.style.stroke = '#212121';
+    plusIcon.style.stroke = '#BDBDBD';
+  }
+};
